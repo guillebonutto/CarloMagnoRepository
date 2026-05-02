b39f6115613f385d3ed0e0eaf9b8e29ae5508000
guillebonutto <guillebonutto@hotmail.com>
2026-05-02 00:45:57 -0300

    Corrigiendo formato STORAGES para Django 5

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -93,17 +93,27 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Configuración de Almacenamiento (Django 5.x)
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Supabase Storage (Nube)
 SUPABASE_URL = os.environ.get('SUPABASE_URL')
 SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
 SUPABASE_BUCKET = os.environ.get('SUPABASE_BUCKET', 'productos')
 
 if SUPABASE_URL and SUPABASE_KEY:
-    DEFAULT_FILE_STORAGE = 'django_supabase_storage.storage.SupabaseStorage'
-    # Opcional: configurar si querés que los archivos sean públicos por defecto
+    STORAGES["default"] = {
+        "BACKEND": "django_supabase_storage.storage.SupabaseStorage",
+    }
     SUPABASE_PUBLIC_URL = True 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
